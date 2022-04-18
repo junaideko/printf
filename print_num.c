ff8d9c7230bcffe686e9138699cfc91acb12ffee
junaideko <junaidekoraimot@gmail.com>
2022-04-18 11:27:21 -0700

    created printf files

---
+++ print_num.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+i/**
+ * print_int - prints an integer
+ * @d: integer to print
+ * Return: number of characters printed
+ */
+
+int print_int(int d)
+{
+	int i, len = 1, num, total = 0;
+	char c;
+
+	if (d < 0)
+	{
+		write(1, "-", 1);
+		total++;
+		d *= -1;
+	}
+
+	num = d;
+
+	for (i = 0; num > 9; i++)
+	{
+		len *= 10;
+		num /= 10;
+	}
+
+	while (i >= 0)
+	{
+		c = d / len + '0';
+		write(1, &c, 1);
+		total++;
+		d %= len;
+		len /= 10;
+		i--;
+	}
+	return (total);
+}
---
+++ printf.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - prints data according to a format
+ * @format: character string composed of zero and directives
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int i, j, d, total = 0;
+	va_list args;
+	char c, *s;
+
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				total++;
+			} else if (format[i] == 'd' || format[i] == 'i')
+			{
+				d = va_arg(args, int);
+				total += print_int(d);
+			} else if (format[i] == 's')
+			{
+				s = va_arg(args, char *);
+				for (j = 0; s[j] != '\0'; j++)
+				{
+					write(1, &s[j], 1);
+					total++;
+				}
+			} else if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				total++;
+			}
+		} else
+		{
+			write(1, &format[i], 1);
+			total++;
+		}
+	}
+	va_end(args);
+	return (total);
+}
