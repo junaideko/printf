fb0a1ce477287bced3d80415e9e30c80a15523b2
Tech-sis <abilawonadeola@gmail.com>
2022-04-19 10:52:05 -0700

    update header file

--- main.h
+++ main.h
@@ -1,7 +1,27 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct specifier
+{
+	char str;
+	int (*func)(va_list *);
+} fmt_spec;
 
 int _printf(const char *format, ...);
-int print_int(int d);
+int print_char(va_list *c);
+int print_str(va_list *str);
+int (*get_func(char s))(va_list *);
+char *_revstr(char *str);
+char *_itoa(unsigned int arg_int, char *strout, int base);
+char *__itoa(int arg_int, char *strout, int base);
+int _strlen(char *str);
+int print_int(va_list *p_int);
+int print_uint(va_list *p_int);
+int print_hex(va_list *p_int);
+int print_oct(va_list *p_int);
 
 #endif
